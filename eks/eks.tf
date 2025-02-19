7aaaf51815ea18ee7545af7f7388c1602f0be17a
idror <144365018+idror67@users.noreply.github.com>
2025-02-19 22:27:25 +0200

    Refactor Terraform and Jenkins configurations; add ServiceMonitor for Flask app and MySQL exporter values

--- eks/eks.tf
+++ eks/eks.tf
@@ -79,19 +79,19 @@ module "eks" {
 
 
 
-resource "aws_db_instance" "default" {
-    allocated_storage    = 20
-    storage_type         = "gp2"
-    engine               = "mysql"
-    engine_version       = "8.0"
-    instance_class       = "db.t4g.micro" 
-    identifier                 = "mydb-terra"
-    username             = "admin"
-    password             = "12345678"
-    publicly_accessible  = true
-    skip_final_snapshot  = true
-    vpc_security_group_ids = ["sg-0b013b8afc4b02785"]
-}
+# resource "aws_db_instance" "default" {
+#     allocated_storage    = 20
+#     storage_type         = "gp2"
+#     engine               = "mysql"
+#     engine_version       = "8.0"
+#     instance_class       = "db.t4g.micro" 
+#     identifier                 = "mydb-terra"
+#     username             = "admin"
+#     password             = "12345678"
+#     publicly_accessible  = true
+#     skip_final_snapshot  = true
+#     vpc_security_group_ids = ["sg-0b013b8afc4b02785"]
+# }
 
 
 
